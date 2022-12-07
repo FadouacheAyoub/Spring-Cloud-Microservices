0e15f4c1f4d38c342c010649a84102c4eb4f67e6
Fadouache Ayoub <ayoubfadouache4@gmail.com>
2022-12-07 09:25:53 +0100

    first commit

---
+++ Customer-service/src/main/java/me/fadouache/Customerservice/CustomerServiceApplication.java
@@ -0,0 +1,31 @@
+package me.fadouache.Customerservice;
+
+import me.fadouache.Customerservice.entities.Customer;
+import me.fadouache.Customerservice.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner start(CustomerRepository cr)
+	{
+		return args -> {
+			Stream.of("Ayoub","Safia", "Sanae", "Ali").forEach(c -> {
+				Customer customer = new Customer();
+				customer.setEmail(c.toLowerCase()+"@gmail.com");
+				customer.setName(c.toUpperCase());
+				cr.save(customer);
+			});
+		};
+	}
+
+}
